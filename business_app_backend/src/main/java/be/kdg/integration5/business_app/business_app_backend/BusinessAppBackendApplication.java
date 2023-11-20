ae02079a024164f41dcc8ba6e82d248841fb892d
Henry Bley <henry.bley@student.kdg.be>
2023-11-20 15:46:31 +0100

    initial

---
+++ business_app_backend/src/main/java/be/kdg/integration5/business_app/business_app_backend/BusinessAppBackendApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.integration5.business_app.business_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAppBackendApplication.class, args);
+	}
+
+}
---
+++ business_app_backend/src/test/java/be/kdg/integration5/business_app/business_app_backend/BusinessAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.integration5.business_app.business_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
